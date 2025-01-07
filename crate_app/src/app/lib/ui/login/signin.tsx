72c68fe56dabf43c029472e6212d979fe03001ae
Louis Decaudaveine <louisdecau@gmail.com>
2025-01-07 21:04:13 +0000

    lets let this shit crash

--- crate_app/src/app/lib/ui/login/signin.tsx
+++ crate_app/src/app/lib/ui/login/signin.tsx
@@ -1,4 +1,4 @@
-// import { signIn, signOut } from "@/auth"
+import { signIn, signOut } from "@/auth"
  
 export function SignInButton() {
   return (
@@ -6,7 +6,7 @@ export function SignInButton() {
       action={async () => {
         "use server"
         console.log("signing in")
-        // await signIn();
+        await signIn();
       }}
     >
       <button className="bg-green-500 text-gray-50 p-2 rounded-lg absolute m-4" type="submit">Sign In</button>
@@ -21,7 +21,7 @@ export function SignOutButton() {
       action={async()=>{
         "use server"
         console.log("signing out")
-        // await signOut()
+        await signOut()
     }}
     >
       <button className="bg-green-500 text-gray-50 p-2 rounded-lg absolute m-4" type="submit">Sign Out</button>
